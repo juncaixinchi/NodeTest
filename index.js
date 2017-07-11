968a2b55778f6fa4faf5da5eadc3caadc975184b
lixinwei <xinwei.li@winsuntech.cn>
2017-07-11 10:32:58 +0800

    update

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const xattr = require('fs-xattr')
 
-const path = './test/a.txt'
+const path = '/media/lxw/Lixw/file_created_in_linux_2.txt'
 
 let list = []
 const attrs = {}
 
-xattr.setSync(path, 'user.key', '233')
-xattr.setSync(path, 'user.author', 'lxw')
+try {
+  xattr.setSync(path, 'user.key', '2333333')
+  xattr.setSync(path, 'user.author', 'juncai')
+  console.log('set xattr success!')
+} catch (e) {
+  console.log('set xattr failed!')
+}
 
-list = xattr.listSync(path)
-
-list.map(attr => ([attr, xattr.getSync(path, attr).toString()])).forEach((arr) => {
-  attrs[arr[0]] = arr[1]
-})
-console.log(attrs)
+try {
+  list = xattr.listSync(path)
+  list.map(attr => ([attr, xattr.getSync(path, attr).toString()])).forEach((arr) => {
+    attrs[arr[0]] = arr[1]
+  })
+  console.log('list xattr success!')
+  console.log('attrs-value: ', attrs)
+} catch (e) {
+  console.log('list xattr failed!')
+}
